a63e0230c1749bdcdf4819c4da99f87d2a13a1fd
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-08 16:25:16 -0300

    Install and config ts-node-dev

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function printHero(hero) {
-    console.log(`Hero: ${hero.alterEgo}, secret identity: ${hero.name}`);
-}
-printHero({
-    name: 'Peter Parker',
-    alterEgo: 'Spider Man'
-});
